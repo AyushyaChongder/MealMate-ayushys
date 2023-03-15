afe897af102a85bdee2cf2392d27ca4f102a5f9f
AyushyaChongder <ayushyachongder@gmail.com>
2023-03-15 09:54:57 +0530

    log in connectivity in progress

--- SQL/user_table.sql
+++ SQL/user_table.sql
@@ -1,5 +1,7 @@
 use mealmate;
+
 show tables;
+
 create table user(
 id  int auto_increment primary key, 
 name varchar(50),
@@ -9,7 +11,36 @@ pincode bigint,
 email varchar(50),
 phone bigint,
 password varchar(20));
+
 ALTER TABLE user AUTO_INCREMENT=1000001;
+
 select * from user;
+
+drop trigger afins;
+drop trigger af_ins;
+drop trigger insert_trigger;
+ truncate table user;
+
+create table master(
+id int primary key,
+name varchar(50),
+email varchar(50),
+password varchar(20),
+user_type varchar(10));
+
+select * from master;
+truncate table master;
+
+delimiter //
+
+CREATE TRIGGER afins AFTER INSERT ON user
+FOR EACH ROW
+INSERT INTO master (id, name, email, password, user_type)
+VALUES (NEW.id, NEW.name, NEW.email, NEW.password, 'User');
+END //
+
+
+
+
 
 
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")//import express module
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const port=4500;
 const mysql=require("./connection").con
 
 
 app.use(express.static('assets'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname + "/views/index.html"))
@@ -29,6 +31,14 @@ app.get("/contact", function(req,res){
 
 app.get("/explore", function(req,res){
     res.sendFile(path.join(__dirname + "/views/shop.html"))
+});
+
+app.get("/success", function(req,res){
+    res.sendFile(path.join(__dirname + "/views/success.html"))
+});
+
+app.get("/loginsuccess", function(req,res){
+    res.sendFile(path.join(__dirname + "/views/loginsuccess.html"))
 });
 
 app.get("/singlepg", function(req,res){
@@ -58,12 +68,54 @@ app.get("/usersignup", (req, res) => {
                     // if (results.affectedRows > 0) {
                     //     res.render("user", { mesg: true })
                     // }
+                    res.sendFile(path.join(__dirname + "/views/success.html"))
                 })
             }
         }
     })
 });
 
+// app.get('/loginperson', function(req, res) {
+//     const email = req.body.email;
+//     const password = req.body.password;
+    
+//     // Check if user exists in the database
+//     connection.query('SELECT * FROM master WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+//       if (error) {
+//         throw error;
+//       }
+//       if (results.length > 0) {
+//         // User authenticated, redirect to the dashboard
+//         res.redirect('/success');
+//       } else {
+//         // Invalid credentials, show error message
+//         res.send('Invalid emailid or password');
+//       }
+//     });
+//   });
+
+app.get("/loginperson", (req, res) => {
+    // fetch data from the form
+
+
+    const { email, password } = req.query;
+
+    let qry = "select * from master where email=? and password=?";
+    mysql.query(qry, [email,password], (err, results) => {
+        if (err) throw err
+        else {
+            if (results.length > 0) {
+                // User authenticated, redirect to the dashboard
+                res.redirect('/loginsuccess');
+              } else {
+                // Invalid credentials, show error message
+                res.send('Invalid emailid or password');
+              }
+
+        }
+        console.log(results)
+    });
+})
 
 app.listen(port,(err)=>{
     if(err)
